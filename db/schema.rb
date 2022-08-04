3a61fdbc595e6865874217b780631085f47e6265
Adam <adamlenter1@gmail.com>
2022-08-04 14:16:34 -0400

    Added models for categories and items.

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_01_182846) do
+ActiveRecord::Schema.define(version: 2022_08_04_164114) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "contacts", force: :cascade do |t|
     t.string "first_name"
@@ -21,6 +27,13 @@ ActiveRecord::Schema.define(version: 2022_08_01_182846) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.integer "category_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Seeding!"
+
+movies = Category.create({name: "Movies"})
+restaurants = Category.create({name: "Restaurants"})
+tv_shows = Category.create({name: "TV Shows"})
+activities = Category.create({name: "Activities"})
+sports_teams = Category.create({name: "Sports Teams"})
+
+Item.create({name: "Titanic", category: movies})
+Item.create({name: "The Big Lebowski", category: movies})
+Item.create({name: "Gino's Pizza", category: restaurants})
+Item.create({name: "McDonald's", category: restaurants})
+Item.create({name: "The Office", category: tv_shows})
+Item.create({name: "Pardon the Interruption", category: tv_shows})
+Item.create({name: "Skiing", category: activities})
+Item.create({name: "Hiking", category: activities})
+
+puts "Done seeding! That will be $8,000"
