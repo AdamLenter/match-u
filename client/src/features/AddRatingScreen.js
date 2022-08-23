47882eacdf7acc8adf90b24277854b9b5f212ab1
Adam <adamlenter1@gmail.com>
2022-08-23 14:59:46 -0400

    Removed unused imports.

--- client/src/features/AddRatingScreen.js
+++ client/src/features/AddRatingScreen.js
@@ -1,5 +1,4 @@
-import React, { useEffect, useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux'
+import React, { useState } from 'react';
 import AddRatingForm from './AddRatingForm'
 import '../App.css';
 import { Link } from 'react-router-dom';
