67633ab6bdba9f04479ae2e4bb23a1bb0030b9c2
Adam <adamlenter1@gmail.com>
2022-08-25 14:06:23 -0400

    Created route for pending matches.

--- client/src/features/NavigationMenu.js
+++ client/src/features/NavigationMenu.js
@@ -9,29 +9,37 @@ function NavigationMenu() {
   return (
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Container>
-        <Navbar.Brand href="#home">Match U</Navbar.Brand>
+        <Navbar.Brand href="/">Match U</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
           </Nav>
+          
           <Nav>
             <NavDropdown title="My Ratings" id="collasible-nav-dropdown">
               <LinkContainer to="/myRatings">
                 <NavDropdown.Item>See my Ratings</NavDropdown.Item>
               </LinkContainer>
+              
               <LinkContainer to="/addARating">
                 <NavDropdown.Item>Add a Rating</NavDropdown.Item>
               </LinkContainer>
             </NavDropdown>
+            
             <NavDropdown title="Matches" id="collasible-nav-dropdown">
               <LinkContainer to="/makeMatch">
                 <NavDropdown.Item>Make a Match</NavDropdown.Item>
               </LinkContainer>
+              
               <LinkContainer to="/receiveMatch">
                 <NavDropdown.Item>Receive a Match</NavDropdown.Item>
               </LinkContainer>
+
+              <LinkContainer to="/pendingMatches">
+                <NavDropdown.Item>My Pending Matches</NavDropdown.Item>
+              </LinkContainer>
+
             </NavDropdown>
-          
           </Nav>
         </Navbar.Collapse>
       </Container>
